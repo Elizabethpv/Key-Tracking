9b176612a6b8c0b25cef75fa2f3fdac0aa2bd206
Elizabethpv <48952600+Elizabethpv@users.noreply.github.com>
2021-03-16 08:16:48 +0530

    commit

---
+++ KeyTracking/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace KeyTracking
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picBasket = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBasket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBasket
+            // 
+            this.picBasket.Image = ((System.Drawing.Image)(resources.GetObject("picBasket.Image")));
+            this.picBasket.Location = new System.Drawing.Point(409, 188);
+            this.picBasket.Name = "picBasket";
+            this.picBasket.Size = new System.Drawing.Size(100, 90);
+            this.picBasket.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBasket.TabIndex = 0;
+            this.picBasket.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 517);
+            this.Controls.Add(this.picBasket);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "KeyTracking";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.picBasket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBasket;
+    }
+}
+
---
+++ KeyTracking/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyTracking
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                picBasket.Left = picBasket.Left - 20;
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                picBasket.Left = picBasket.Left + 20;
+            }
+
+            if (e.KeyCode == Keys.C) 
+            {
+                picBasket.Location = new Point(409, 188);
+               
+            }
+
+            if (e.KeyCode == Keys.T)
+            {
+                picBasket.Top = picBasket.Top - 20;
+
+            }
+
+            if (e.KeyCode == Keys.B)
+            {
+                picBasket.Top = picBasket.Top + 20;
+            }
+        }
+    }
+}
